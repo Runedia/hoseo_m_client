0fc8b0ad6dcc786c1f0afff1b845bc055e7cf0dc
swsw12 <163833032+swsw12@users.noreply.github.com>
2025-05-25 13:01:46 +0900

    mainscreen, notice_screen 개발

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'notice_screen.dart';
+import 'shuttle_screen.dart'; 
+import 'shuttle_select_screen.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +10,127 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '호서대학교 공지앱',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final List<_FeatureItem> topFeatures = const [
+    _FeatureItem("공지사항", Icons.campaign),
+    _FeatureItem("셔틀버스", Icons.directions_bus),
+    _FeatureItem("학과정보", Icons.school),
+    _FeatureItem("식단표", Icons.restaurant_menu),
+    _FeatureItem("캠퍼스맵", Icons.map),
+    _FeatureItem("학사종합", Icons.book),
+  ];
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<_FeatureItem> bottomFeatures = const [
+    _FeatureItem("LMS", Icons.laptop),
+    _FeatureItem("통합포털", Icons.web),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final size = MediaQuery.of(context).size;
+    final double iconSize = size.width * 0.12;
+    final double fontSize = size.width * 0.035;
+    final double spacing = size.width * 0.025;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("호서대학교 앱"),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(spacing),
+          child: Column(
+            children: [
+              Wrap(
+                spacing: spacing,
+                runSpacing: spacing,
+                alignment: WrapAlignment.center,
+                children: topFeatures.map((item) {
+                  return SizedBox(
+                    width: (size.width - spacing * 4) / 3,
+                    height: size.width * 0.25,
+                    child: _buildCard(context, item, iconSize, fontSize),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: spacing * 2),
+              Column(
+                children: bottomFeatures.map((item) {
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: spacing),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.95,
+                      child: _buildCard(context, item, iconSize, fontSize),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _buildCard(BuildContext context, _FeatureItem item, double iconSize, double fontSize) {
+    return Card(
+      color: Colors.red[700],
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: InkWell(
+        onTap: () {
+          if (item.title == "공지사항") {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const NoticeScreen()),
+            );
+          } else if (item.title == "셔틀버스") {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const ShuttleSelectScreen()),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('${item.title} 클릭됨')),
+            );
+          }
+        },
+        borderRadius: BorderRadius.circular(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(item.icon, size: iconSize, color: Colors.white),
+              SizedBox(height: iconSize * 0.15),
+              Text(item.title,
+                  style: TextStyle(fontSize: fontSize, color: Colors.white)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _FeatureItem {
+  final String title;
+  final IconData icon;
+  const _FeatureItem(this.title, this.icon);
 }
