aead7f88b55070513d32ccda655662e865aaece1
swsw12 <163833032+swsw12@users.noreply.github.com>
2025-06-03 12:20:46 +0900

    수정사항반영

--- lib/notice_screen.dart
+++ lib/notice_screen.dart
@@ -147,6 +147,8 @@ class _NoticeScreenState extends State<NoticeScreen> {
                               builder: (_) => NoticeWebViewPage(
                                 title: detail['title'] ?? '',
                                 url: fullUrl,
+                                userAgent:
+                                'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1',
                               ),
                             ),
                           );
